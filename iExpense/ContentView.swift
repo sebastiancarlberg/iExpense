3eb0172eda51052fffd2e32adc83127a6f5905a7
Sebastian Carlberg <carlberg.sebastian@gmail.com>
2020-01-28 19:54:15 +0100

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Sebastian Carlberg on 2020-01-28.
+//  Copyright © 2020 Sebastian Carlberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
